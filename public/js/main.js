23bb6e4324cfde472beafe5cc1c2c1b3abfe9cab
Jafari Mchasa Akida <jakida@tucode.co.ke>
2024-09-26 13:52:14 +0300

    updated

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,10 @@
         }, 1);
     };
     spinner(0);
-
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
 
     // Sticky Navbar
     $(window).scroll(function () {
@@ -22,53 +25,43 @@
     });
 
 
-    // International Tour carousel
-    $(".InternationalTour-carousel").owlCarousel({
+    // Hero Header carousel
+    $(".header-carousel").owlCarousel({
+        animateOut: 'fadeOut',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
         autoplay: true,
-        smartSpeed: 1000,
-        center: false,
+        smartSpeed: 500,
         dots: true,
         loop: true,
-        margin: 25,
-        nav : false,
+        nav : true,
         navText : [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
-        responsiveClass: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:3
-            }
-        }
     });
 
 
-    // packages carousel
-    $(".packages-carousel").owlCarousel({
+    // attractions carousel
+    $(".blog-carousel").owlCarousel({
         autoplay: true,
-        smartSpeed: 1000,
+        smartSpeed: 1500,
         center: false,
         dots: false,
         loop: true,
         margin: 25,
         nav : true,
         navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
+            '<i class="fa fa-angle-right"></i>',
+            '<i class="fa fa-angle-left"></i>'
         ],
         responsiveClass: true,
         responsive: {
             0:{
+                items:1
+            },
+            576:{
                 items:1
             },
             768:{
@@ -87,19 +80,22 @@
     // testimonial carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
-        smartSpeed: 1000,
-        center: true,
+        smartSpeed: 1500,
+        center: false,
         dots: true,
         loop: true,
         margin: 25,
         nav : true,
         navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
+            '<i class="fa fa-angle-right"></i>',
+            '<i class="fa fa-angle-left"></i>'
         ],
         responsiveClass: true,
         responsive: {
             0:{
+                items:1
+            },
+            576:{
                 items:1
             },
             768:{
@@ -114,7 +110,14 @@
         }
     });
 
-    
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 5,
+        time: 2000
+    });
+
+
    // Back to top button
    $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
@@ -126,7 +129,8 @@
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
-    }); 
+    });
+
 
 })(jQuery);
 
